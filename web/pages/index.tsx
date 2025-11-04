133b8bbc300bbcf852284e88b2af58a6320b3b7e
Vince <vincelm3@gmail.com>
2025-11-04 15:31:48 -0600

    web: update homepage title to Mico's World

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: 20 }}>
-      <h1>HeyAnon Web</h1>
+      <h1>Mico's World</h1>
       <p>If you see this page, Next.js is running.</p>
       <p>
         <a href="/strategies">View strategies</a> |{" "}
